5a7904dcda9d4275adb8a3d8819ca17fff1360d8
Gabriel Khot Garang Pawuoi <141131685+KhotKeys@users.noreply.github.com>
2024-05-19 07:17:23 -0700

    Update wise_men_from_Africa.py

--- wise_men_from_Africa.py
+++ wise_men_from_Africa.py
@@ -1,32 +1,29 @@
-
-def integers_file(file):
+def integers_file(file_name):
     try:
-        with open(file, 'r') as file:
-            integers = file.readlines()
-            integers = [int(line.strip()) for line in integers]
-        return integers
+        with open(file_name, 'r') as file_object:
+            unique_integers_flow = file_object.readlines()
+            nique_integers_flow  = [int(line.strip()) for line in unique_integers_flow]
+        return nique_integers_flow
     except Exception as e:
-        print(f"Error reading the file: {e}")
+        print(f"When there is an error in the file: {e}")
         return []
 
 
 def get_unique_sorted_integers(integers):
     unique_integers = list(set(integers))
-
+   
     unique_integers.sort()
-
+   
     return unique_integers
 
-
 def main():
-    file = r'uniquelnt.txt'
-    integers = integers_file(file)
-
+    file_name = r'uniquelnt.txt' 
+    integers = integers_file(file_name)
+    
     if integers:
         unique_sorted_integers = get_unique_sorted_integers(integers)
         for integer in unique_sorted_integers:
             print(integer)
-
-
+            
 if __name__ == "__main__":
     main()
